49e4edd8265a657fe5712775696095a55f3e2fc6
Ivaylo Georgiev <ivailo1411@gmail.com>
2024-07-12 22:24:17 +0300

    Detailed bug fix

--- template/src/requests/request-service.js
+++ template/src/requests/request-service.js
@@ -41,3 +41,5 @@ export const uploadFile = async (input) => {
   }
   
 }
+
+// VbnUQpnihPSIgIXuZv
--- template/src/views/detailed-view.js
+++ template/src/views/detailed-view.js
@@ -5,7 +5,7 @@ export const toDetailedView = (gif) => `
     </div>
     <div class="detailed-info">
         <div class="user-avatar">
-            <img src="${gif.data.user.avatar_url}">
+            <img src="${gif.data.user !== undefined ? gif.data.user.avatar_url : `https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png`}">
             <h3>${gif.data.username}</h3>
         </div>
         <div class="gif-info">
