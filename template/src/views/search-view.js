ec8b57edb4395c3919e991251a2ca969e31c9d9f
Ivaylo Georgiev <ivailo1411@gmail.com>
2024-07-12 20:17:08 +0300

    Style fixes Upload and Search View

--- template/src/views/search-view.js
+++ template/src/views/search-view.js
@@ -1,14 +1,25 @@
 export const toSearchView = (gifs, searchTerm) => `
 <div class="search-heading"><h1>Gifs found for: ${searchTerm}</h1></div><br>
-<div id="search-content">
+${gifs.data.length > 0 ? `
+    <div id="search-content">
+        ${gifs.data
+            .map((gif) => `
+            <div class="search-item">
+                <img class="gif" data-gif-id="${gif.id}" src="${gif.images.downsized_medium.url}">
+            </div>`).join("\n")}
+    </div>` : `
+    <div id="search-empty">
+        <p>Search for something!</p>
+    </div> `}
+`;
+
+
+{/* <div id="search-content">
     ${gifs.data
-        .map(
-          (gif) => `
+        .map((gif) => `
     <div class="search-item">
         <img class="gif" data-gif-id="${gif.id}" src="${gif.images.downsized_medium.url}">
-    </div>`
-        )
-        .join("\n") || "<p>Search for something</p>"
+    </div>`)
+        .join("\n") || "<div></div><p>Search for something!</p>"
     }
-</div>
-`;
+</div> */}
