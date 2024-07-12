63df7c53a1030b3f397f6f61d3d8b62c86e123d2
Ivaylo Georgiev <ivailo1411@gmail.com>
2024-07-12 22:34:13 +0300

    User avatar on Profile change

--- template/src/views/profile-view.js
+++ template/src/views/profile-view.js
@@ -6,7 +6,8 @@ export const toProfileView = (uploadedGifs) => `
     </div>
     
     <div class="about-user">
-      <h1>Your Profile:</h1>
+      <h1>Your Profile:</h1>\
+      <img src="https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/118947646/original/9fb85fe56953295c5592270439d44b477c742ca5/create-a-pixel-art-charakter-for-you.png" alt="profile-picture">
       <div class="user-data">
       <h3> Username: GifLover2 </h3>
       <h4> Join Date: 10.07.2024 </h4>
