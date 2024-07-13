0bb3e73f4e7aaa0a9a19bcea7659e311f433cb9b
Daniel Stoyanov <stoyanov_d@hotmail.com>
2024-07-13 17:09:52 +0300

    added documentation to favorites file

--- template/src/data/favorites.js
+++ template/src/data/favorites.js
@@ -1,5 +1,18 @@
+/**
+ * @file Manages favorite GIDs using localStorage.
+ */
+
+/**
+ * An array of favorite GIF IDs.
+ * @type {string[]}
+ */
 let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
 
+/**
+ * Adds a GIF ID to the favorites list. If the list is empty, it adds the ID. 
+ * If the list is not empty, it replaces the existing ID with the new one.
+ * @param {string} gifID - The ID of the GIF to add to favorites. 
+ */
 export const addFavorite = (gifID) => {
   if (favorites.length === 0) {
     favorites.push(gifID);
@@ -10,10 +23,17 @@ export const addFavorite = (gifID) => {
     localStorage.setItem('favorites', JSON.stringify(favorites));
   }
 };
-
+/**
+ * Removes a GIF ID from favorites list.
+ * @param {string} gifId - The ID of the GIF to remove from favorites. 
+ */
 export const removeFavorite = (gifId) => {
   favorites = favorites.filter(id => id !== gifId);
   localStorage.setItem('favorites', JSON.stringify(favorites));
 };
 
+/**
+ * Gets the list of favorite GIF IDs
+ * @returns {string[]} The list of favorite GIF IDs.
+ */
 export const getFavorites = () => [...favorites];
