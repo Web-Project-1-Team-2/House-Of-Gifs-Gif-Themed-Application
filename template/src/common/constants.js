2d133ea28a4935532a2eaa041a49c5016425a92f
Daniel Stoyanov <stoyanov_d@hotmail.com>
2024-07-13 17:00:40 +0300

    documentation added for constant file

--- template/src/common/constants.js
+++ template/src/common/constants.js
@@ -1,3 +1,6 @@
+/**
+ * A class representing application constants.
+ */
 export class Constants {
     #LIMIT = 35;
     #API_KEY = 'UsqNJhspAhQU1sNdZ4nUmmQbj0hfcDLU'
@@ -6,27 +9,69 @@ export class Constants {
     //2nd API: iRc8ZKeJlIPBCr51asOvxBGMW1YTLAoD
     //3nd API: UsqNJhspAhQU1sNdZ4nUmmQbj0hfcDLU
 
+    /**
+     * Gets the limit constant.
+     * @returns{number} The limit constant.
+     */
     get LIMIT (){
         return this.#LIMIT;
     }
 
+    /**
+     * Gets the API key
+     * @returns {string} The API key.
+     */
     get API_KEY (){
         return this.#API_KEY;
     }
 }
-
+/**
+ * @constant {string}
+ * @default
+ */
 export const TRENDS = 'trends';
 
+/**
+ * @constant {string}
+ * @default
+ */
 export const UPLOAD = 'upload';
 
+/**
+ * @constant {string}
+ * @default
+ */
 export const FAVORITES = 'favorites';
 
+/**
+ * @constant {string}
+ * @default
+ */
 export const ABOUT = 'about';
 
+/**
+ * @constant {string}
+ * @default
+ */
 export const PROFILE = 'profile';
 
+/**
+ * The CSS selector for the container element.
+ * @constant {string}
+ * @default
+ */
 export const CONTAINER_SELECTOR = '#container';
 
+/**
+ * Full heart symbol.
+ * @constant {string}
+ * @default
+ */
 export const FULL_HEART = '❤';
 
+/**
+ * Empty hear symbol.
+ * @constant {string}
+ * @default
+ */
 export const EMPTY_HEART = '♡';
