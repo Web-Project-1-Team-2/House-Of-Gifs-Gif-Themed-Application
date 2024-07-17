4acb057978a459fb877e23dcd8aef34f3aea1754
Ivaylo Georgiev <ivailo1411@gmail.com>
2024-07-17 11:10:04 +0300

    Upload Change

--- template/src/views/upload-view.js
+++ template/src/views/upload-view.js
@@ -7,7 +7,7 @@ export const toUploadView = () => `
   <h1>Upload your Gif:</h1>
   <div class ="form">
     <form id="upload-form">
-    <input type="file" id="upload-file">
+    <input type="file" id="upload-file" accept="image/gif">
     <button class="upload-button">Upload</button>
     </form>
   </div>
